c76996c0a2006ed232ccedde8466c3a912c82c20
lyyyuna <lyyyuna@outlook.com>
2019-08-23 09:56:42 +0800

    update

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"net/http"
 	"time"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
@@ -22,17 +23,18 @@ func main() {
 
 	bot.Debug = true
 	request := gorequest.New()
-	i := 1000
+	i := 1500
 	for {
 		j := 1
 		for {
 			targetUrl := fmt.Sprintf("https://img1.mm131.me/pic/%v/%v.jpg", i, j)
-			_, body, _ := request.Get(targetUrl).
+			resp, body, _ := request.Get(targetUrl).
 				Set("Referer", "https://m.mm131.net/xinggan").
 				Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36").
 				Set("Sec-Fetch-Mode", "no-cors").End()
-			if err != nil {
-				glog.Errorf("Failed to generate doc from body, the error is: %v", err)
+
+			if resp.StatusCode == http.StatusNotFound {
+				glog.Errorf("404 not found, %v - %v", i, j)
 				break
 			}
 
@@ -41,12 +43,14 @@ func main() {
 				Bytes: []byte(body)}
 			msg := tgbotapi.NewPhotoUpload(-1001257795899, b)
 			msg.Caption = fmt.Sprintf("Sweet ~~ %v-%v", i, j)
-			_, err = bot.Send(msg)
+			_, err := bot.Send(msg)
 			if err != nil {
 				glog.Errorf("Send image error, the error is: %v", err)
+				glog.Error(resp.StatusCode)
+				glog.Errorf("%v - %v", i, j)
 			}
 
-			time.Sleep(time.Second * 360)
+			time.Sleep(time.Second * 30)
 
 			j++
 		}
